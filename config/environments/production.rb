1f07eb2ea16635a6a1f026f7a11b20090421abc2
Steven Petryk <petryk.steven@gmail.com>
2018-03-10 14:29:06 -0500

    Use redistogo url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.web_socket_server_url = ENV.fetch("ACTIONCABLE_URL")
-  config.action_cable.allowed_request_origins = [
-    "https://partygoer.io",
-    "https://partygoer-web.herokuapp.com",
-  ]
+  config.action_cable.disable_request_forgery_protection = true
 end
