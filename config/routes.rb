a4481474bdcd3133ae6be1c1bac22e03d3fb0594
Steven Petryk <petryk.steven@gmail.com>
2018-03-10 19:26:28 -0500

    Add sidekiq monitor

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
   get "/preauth/spotify", to: "sessions#spotify_preauth"
   get "/auth/spotify/callback", to: "sessions#spotify_callback"
@@ -7,4 +9,6 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
+
+  mount Sidekiq::Web => "/sidekiq"
 end
